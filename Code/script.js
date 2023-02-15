e4b95ae972e89779adc132ea86695b7dc56acf23
Dafzl <124206445+Dafzl@users.noreply.github.com>
2023-02-15 21:51:25 +0100

    Add files via upload

---
+++ Code/script.js
@@ -0,0 +1,7 @@
+var navlist = document.querySelector('.nav-list');
+var links = document.querySelector('.nav-list li ');
+
+hamburger.addEventListener('click', function(){
+    this.classList.toggle('click');
+    navlist.classList.toggle('open');
+});
